b9b023c16519b5c806971251115303b2af6ef2e0
Moritz <moritz-hier@outlook.de>
2026-06-08 22:16:20 +0200

    Fix Leaflet marker icons

--- src/MapView.js
+++ src/MapView.js
@@ -1,6 +1,17 @@
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import { supabase } from './supabase.js'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow
+})
 
 export function createMap() {
   let selectedLocation = null
